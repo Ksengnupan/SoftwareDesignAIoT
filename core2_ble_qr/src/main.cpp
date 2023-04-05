c92bdd285e44c65552d828040c4d2ad8e9c56fcd
Hnyot Myat Wunn <hnyotmyetwunnshunnle.m@live.ku.th>
2023-04-05 17:40:10 +0700

    Change text location

--- core2_ble_qr/src/main.cpp
+++ core2_ble_qr/src/main.cpp
@@ -10104,8 +10104,8 @@ void loop() {
                 M5.Lcd.setTextSize(3);
                 M5.Lcd.setCursor(50, 0);
                 M5.Lcd.println("Scan Here For");
-                M5.Lcd.setCursor(120, 200);
-                M5.Lcd.println("MENU");
+                M5.Lcd.setCursor(110, 200);
+                M5.Lcd.println(" MENU ");
                 M5.Lcd.qrcode("https://github.com/m5stack/M5Core2/blob/master/examples/Basics/speak/speak.ino", 75, 28, 170, 6);
                 //M5.Lcd.qrcode("https://www.youtube.com/watch?v=_EUH4blVQ0I&ab_channel=EdSheeran", 50, 10, 225, 6);
                 delay(5000);
